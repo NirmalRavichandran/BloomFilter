a12c77d649ad7f8bcfbd7b2d36e62d4453566078
NirmalRavichandran <nirmal18ravi@gmail.com>
2026-01-17 23:41:01 +0530

    Completed the bloom filter functionalities

--- bloom.go
+++ bloom.go
@@ -1,21 +1,42 @@
 package bloom
 
-import "hash"
+import (
+	"hash"
+)
 
 type bloomFilter struct {
-	bitMap []uint32
-	k      int
-	m      int
-	n      int
+	bitMap []bool // bit slice
+	k      int    // number of hash functions
+	m      int    // size of the filter
+	n      int    // number of elements in the filter
 	hashfn hash.Hash64
 }
 
-func NewBloomFilter() *bloomFilter {
-	return &bloomFilter{}
+func NewBloomFilter(noHashes, filterSize int) *bloomFilter {
+	bf := new(bloomFilter)
+	bf.bitMap = make([]bool, filterSize)
+	bf.k = noHashes
+	bf.m = filterSize
+	bf.n = 0 // initial value before adding any element
+	return bf
 }
 
-func (bf *bloomFilter) Add(element []byte) {}
+func (bf *bloomFilter) Add(element []byte) {
+	h1, h2 := bf.getHash(element)
+	for i := 0; i < bf.k; i++ {
+		index := (h1 + uint32(i)*h2) % uint32(bf.m)
+		bf.bitMap[index] = true
+	}
+	bf.n++
+}
 
 func (bf *bloomFilter) Check(element []byte) bool {
-	return false
+	h1, h2 := bf.getHash(element)
+	for i := 0; i < bf.k; i++ {
+		index := (h1 + uint32(i)*h2) % uint32(bf.m)
+		if !bf.bitMap[index] {
+			return false
+		}
+	}
+	return true
 }
--- hash.go
+++ hash.go
@@ -1,1 +1,10 @@
 package bloom
+
+func (bf *bloomFilter) getHash(element []byte) (uint32, uint32) {
+	bf.hashfn.Reset()
+	bf.hashfn.Write(element)
+	hash := bf.hashfn.Sum64()
+	h1 := uint32(hash & 0xFFFFFFFF)
+	h2 := uint32(hash >> 32)
+	return h1, h2
+}
